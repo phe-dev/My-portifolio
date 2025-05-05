9ca7875f3be77e38979287bf9f7acb98353d06ae
Pedro Henrique <pedromontoyaph7@ufpi.edu.br>
2025-05-04 22:18:28 -0300

    subindo os arquivos

---
+++ www.phe.dev/js/particles-config.js
@@ -0,0 +1,52 @@
+
+{
+  "particles": {
+    "number": {
+      "value": 60
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
+    },
+    "modes": {
+      "repulse": {
+        "distance": 100
+      },
+      "push": {
+        "quantity": 4
+      }
+    }
+  },
+  "retina_detect": true
+}
